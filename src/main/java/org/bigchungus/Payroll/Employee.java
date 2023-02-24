bbadf9b821e1dc83972d6103bf87cbe9981bc5c5
Alex <rereon63@gmail.com>
2023-02-24 16:46:46 -0500

    add Employee firstName & lastName

--- src/main/java/org/bigchungus/Payroll/Employee.java
+++ src/main/java/org/bigchungus/Payroll/Employee.java
@@ -4,17 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 
 @Entity
 public class Employee {
     private @Id @GeneratedValue Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String role;
 
     Employee() {}
 
-    Employee(String name, String role) {
-        this.name = name;
+    Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
@@ -23,9 +27,14 @@ public class Employee {
     }
 
     public String getName() {
-        return this.name;
+        return this.firstName + " " + this.lastName;
     }
-
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
     public String getRole() {
         return this.role;
     }
@@ -33,18 +42,25 @@ public class Employee {
     public void setId(Long id) {
         this.id = id;
     }
-
     public void setName(String name) {
-        this.name = name;
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
-
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     public void setRole(String role) {
         this.role = role;
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+                + '\'' + ", role='" + this.role + '\'' + '}';
     }
 
     @Override
@@ -54,11 +70,14 @@ public class Employee {
         if(!(o instanceof Employee))
             return false;
         Employee employee = (Employee) o;
-        return this.id.equals(employee.id) && this.name.equals(employee.name) && this.role.equals(employee.role);
+        return Objects.equals(this.id, employee.id) &&
+                Objects.equals(this.firstName, employee.firstName) &&
+                Objects.equals(this.lastName, employee.lastName) &&
+                Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return java.util.Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 }
